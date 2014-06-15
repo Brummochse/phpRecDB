90c10391b5740eb5f9e3a981598f9ef9634cb037
DrEvilMobil <DrEvilMobil@DrEvilMobilPC>
2014-06-15 02:35:48 +0200

    - 1.1 test 5

--- phpRecDB/app/common/config/params.php
+++ phpRecDB/app/common/config/params.php
@@ -2,6 +2,6 @@
 
 return array(
     'screenshotsUrl' => $phpRecDbUrl .(empty($phpRecDbUrl)?'':'/'). 'screenshots',
-    'version' => '1.1 Test 4',
+    'version' => '1.1 Test 5',
     'wwwUrl' => $phpRecDbUrl .(empty($phpRecDbUrl)?'':'/') . 'app/www'
 );
