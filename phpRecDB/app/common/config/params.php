e7b9c5f755cd3549dc78e066c480dce9be7676a8
DrEvilMobil <DrEvilMobil@DrEvilMobilPC>
2014-07-08 03:53:04 +0200

    - add submenus to artistmenu

--- phpRecDB/app/common/config/params.php
+++ phpRecDB/app/common/config/params.php
@@ -3,5 +3,6 @@
 return array(
     'screenshotsUrl' => $phpRecDbUrl .(empty($phpRecDbUrl)?'':'/'). 'screenshots',
     'version' => '1.1',
-    'wwwUrl' => $phpRecDbUrl .(empty($phpRecDbUrl)?'':'/') . 'app/www'
+    'wwwUrl' => $phpRecDbUrl .(empty($phpRecDbUrl)?'':'/') . 'app/www',
+    'artistMenuMaxChunkSize'=>20
 );
