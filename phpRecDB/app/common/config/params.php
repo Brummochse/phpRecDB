20a7744d3933e832ab4bd46958aed88d8846fb40
mk <mikl@fh-zwickau.de>
2019-02-02 15:17:15 +0100

    fix wants list date

--- phpRecDB/app/common/config/params.php
+++ phpRecDB/app/common/config/params.php
@@ -2,7 +2,7 @@
 
 return array(
     'screenshotsUrl' => $phpRecDbUrl .(empty($phpRecDbUrl)?'':'/'). 'screenshots',
-    'version' => '1.2',
+    'version' => '1.3 Test 1',
     'wwwUrl' => $phpRecDbUrl .(empty($phpRecDbUrl)?'':'/') . 'app/www',
     'artistMenuMaxChunkSize'=>20
 );
