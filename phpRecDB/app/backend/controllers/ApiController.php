5ac02805059f532b49b1f5b642fc71be0da866d1
mikl <mikl@fh-zwickau,de>
2021-03-12 11:44:17 +0100

    cleanup Rest API

--- phpRecDB/app/backend/controllers/ApiController.php
+++ phpRecDB/app/backend/controllers/ApiController.php
@@ -3,11 +3,6 @@
 
 class ApiController extends CController
 {
-// Members
-    /**
-     * Key which has to be in HTTP USERNAME and PASSWORD headers
-     */
-    Const APPLICATION_ID = 'ASCCPE';
 
     /**
      * @return array action filters
@@ -20,10 +15,11 @@ class ApiController extends CController
     /**
      * @var RestEndpoint[]
      */
-    private $restEndpoints=[];
+    private $restEndpoints = [];
 
-    private function AddRestEndpoint(RestEndpoint $endpoint) {
-        $this->restEndpoints[$endpoint->getName()]=$endpoint;
+    private function AddRestEndpoint(RestEndpoint $endpoint)
+    {
+        $this->restEndpoints[$endpoint->getName()] = $endpoint;
 
     }
 
@@ -34,41 +30,40 @@ class ApiController extends CController
         $this->AddRestEndpoint(new InfoEndpoint());
     }
 
-    private function getEndpoint(string $name) {
-        if (!key_exists($name,$this->restEndpoints)) {
+    private function getEndpoint(string $name)
+    {
+        if (!key_exists($name, $this->restEndpoints)) {
             throw new BadMethodCallException();
         }
         return $this->restEndpoints[$name];
     }
 
-    private function processRestAction(callable  $executable) {
+    private function processRestAction(callable $executable)
+    {
         try {
-            $resultArray=$executable();
-            $encode = CJSON::encode($resultArray);
-            $this->_sendResponse(200,$encode,'application/json');
-        }
-        catch(BadMethodCallException $e)
-        {
-            $this->_sendResponse(501);
-        }
-        catch(Exception $e)
-        {
-            $this->_sendResponse(500);
+            if (!$this->checkAuthentication()) {
+                $this->sendResponse(401);
+            }
+            $resultArray = $executable();
+            $encode = empty($resultArray) ?'' : CJSON::encode($resultArray);
+            $this->sendResponse(200, $encode, 'application/json');
+        } catch (BadMethodCallException $e) {
+            $this->sendResponse(501);
+        } catch (Exception $e) {
+            $this->sendResponse(500);
         }
     }
 
     public function actionList()
     {
-        $this->processRestAction(function ()
-        {
+        $this->processRestAction(function () {
             return $this->getEndpoint($_GET['model'])->list();
         });
     }
 
     public function actionView()
     {
-        $this->processRestAction(function ()
-        {
+        $this->processRestAction(function () {
             return $this->getEndpoint($_GET['model'])->view();
         });
 
@@ -76,9 +71,7 @@ class ApiController extends CController
 
     public function actionCreate()
     {
-
-        $this->processRestAction(function ()
-        {
+        $this->processRestAction(function () {
             return $this->getEndpoint($_GET['model'])->create();
         });
 
@@ -86,42 +79,36 @@ class ApiController extends CController
 
     public function actionUpdate()
     {
-        $this->processRestAction(function ()
-        {
+        $this->processRestAction(function () {
             return $this->getEndpoint($_GET['model'])->update();
         });
-
     }
 
     public function actionDelete()
     {
-        $this->processRestAction(function ()
-        {
+        $this->processRestAction(function () {
             return $this->getEndpoint($_GET['model'])->delete();
         });
     }
 
-    private function _sendResponse($status = 200, $body = '', $content_type = 'text/html')
+    private function sendResponse($status = 200, $body = '', $content_type = 'text/html')
     {
         $status_header = 'HTTP/1.1 ' . $status . ' ' . $this->_getStatusCodeMessage($status);
         header($status_header);
         header('Content-type: ' . $content_type);
 
-        if($body != '')
-        {
+        if ($body != '') {
             echo $body;
-        }
-        else
-        {
+        } else {
             $message = $this->_getStatusCodeMessage($status);
-            $this->renderPartial('error', array('status'=>$status,'message'=>$message));
+            $this->renderPartial('response', array('status' => $status, 'message' => $message));
         }
         Yii::app()->end();
     }
 
     private function _getStatusCodeMessage($status)
     {
-        $codes = Array(
+        $codes = array(
             200 => 'OK',
             400 => 'Bad Request',
             401 => 'Unauthorized',
@@ -134,23 +121,21 @@ class ApiController extends CController
         return (isset($codes[$status])) ? $codes[$status] : '';
     }
 
-    private function _checkAuth()
+    private function checkAuthentication()
     {
-        // Check if we have the USERNAME and PASSWORD HTTP headers set?
-        if(!(isset($_SERVER['HTTP_X_USERNAME']) and isset($_SERVER['HTTP_X_PASSWORD']))) {
-            // Error: Unauthorized
-            $this->_sendResponse(401);
+        if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
+            $user = $_SERVER['PHP_AUTH_USER'];
+            $password = $_SERVER['PHP_AUTH_PW'];
+
+            $userIdentity = new UserIdentity($user, $password);
+
+            try {
+                if ($userIdentity->authenticate() && $userIdentity->getState("roles") == "admin") {
+                    return true;
+                }
+            } catch (CException $e) {
+            }
         }
-        $username = $_SERVER['HTTP_X_USERNAME'];
-        $password = $_SERVER['HTTP_X_PASSWORD'];
-        // Find the user
-        $user=User::model()->find('LOWER(username)=?',array(strtolower($username)));
-        if($user===null) {
-            // Error: Unauthorized
-            $this->_sendResponse(401, 'Error: User Name is invalid');
-        } else if(!$user->validatePassword($password)) {
-            // Error: Unauthorized
-            $this->_sendResponse(401, 'Error: User Password is invalid');
-        }
+        return false;
     }
 }
---
+++ phpRecDB/app/backend/views/api/response.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title><?= $status; ?> <?= $message; ?></title>
+</head>
+<body>
+<h1><?= $status; ?> <?= $message; ?></h1>
+</body>
+</html>
+
