3852e333c9452be7ecbfc0eb130dcec3240ab4ff
mikl <mikl@fh-zwickau,de>
2021-03-28 21:07:09 +0200

    final version 1.3

--- phpRecDB/app/common/config/params.php
+++ phpRecDB/app/common/config/params.php
@@ -2,7 +2,7 @@
 
 return array(
 
-    'version' => '1.3 Test 5 (2021-03-22)',
+    'version' => '1.3',
     'artistMenuMaxChunkSize'=>20,
     'wwwUrl' => '<placeholder to fill in setWwwUrl Filter in Controller>',
     'screenshotsUrl' => '<placeholder to fill in setWwwUrl Filter in Controller>',
