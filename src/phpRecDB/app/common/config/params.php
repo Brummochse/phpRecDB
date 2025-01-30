c1e13bf85d2a1b04b82cc43b1750458feb595b71
mk <der-doktor@gmx.de>
2025-01-30 23:36:15 +0100

    change version and changelist

--- src/phpRecDB/app/common/config/params.php
+++ src/phpRecDB/app/common/config/params.php
@@ -2,7 +2,7 @@
 
 return array(
 
-    'version' => '1.3.1',
+    'version' => '1.4',
     'artistMenuMaxChunkSize'=>20,
     'wwwUrl' => '<placeholder to fill in setWwwUrl Filter in Controller>',
     'screenshotsUrl' => '<placeholder to fill in setWwwUrl Filter in Controller>',
